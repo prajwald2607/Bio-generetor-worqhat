ccd88718d8e4cc03d91e62684c8d9eab53a117a0
Prajwal Deshmukh <smartpd2607@gmail.com>
2023-06-20 20:05:53 +0530

    styles-updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from './layout';
-import '../app/globals.css';
+import '../app/globals.css';  
+import { TbSquareRoundedNumber1Filled } from "react-icons/tb";
+import { TbSquareRoundedNumber2Filled } from "react-icons/tb";
 const MyPage = () => {
   return (
     <Layout>
@@ -8,14 +10,20 @@ const MyPage = () => {
         <div>
         <h1>Generate your next Twitter bio using Chatgpt</h1>
         </div>
-        
+
+        <div className="icon-heading">
+        <TbSquareRoundedNumber1Filled size={32}></TbSquareRoundedNumber1Filled>
         <h2 className="steps">Copy your current bio (or write few sentences about yourself).</h2>
+        </div>
+        
       <textarea
         className="my-textarea"
         placeholder="eg. Senior Developer Advocate @Worqhat. Tweeting about web development, AI, and React / Next.js, Writing nutlope.substack.com."
       />
-      <div>
+      <div className="icon-heading">
+      <TbSquareRoundedNumber2Filled size={32}></TbSquareRoundedNumber2Filled>
       <h2 className="steps">Select your vibe</h2>
+      </div><div>
       <select className="menu">
         <option value="">Select an option</option>
         <option value="twitter" className="stylesoption">Twitter</option>
@@ -24,7 +32,6 @@ const MyPage = () => {
     </div>
     <button className="generatebtn">Generate your bio</button>
       </div>
-
     </Layout>
   );
 };
